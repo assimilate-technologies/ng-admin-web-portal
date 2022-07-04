1aba03cecaec131670bc49b88ace864398aa72d7
vaibhav-ingle <vingle@assimilatetechnologies.com>
2022-07-04 17:16:44 +0530

    menu

--- src/app/layout/components/sidebar/models.ts
+++ src/app/layout/components/sidebar/models.ts
@@ -1,4 +1,4 @@
 export const MenuItem = [
     { routerLink: '/dashboard', icon: 'fa-dashboard', routeName: 'Dashboard', submenu: [] },
-    { routerLink: '/users/profile', icon: 'fa-user', routeName: 'Profile', submenu: [  { routerLink: '/users/profile', icon: 'fa-user', routeName: 'Profile' }] }
+    { routerLink: '/users/profile', icon: 'fa-user', routeName: 'Profile', submenu: [ ]}
 ];
