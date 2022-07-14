6f78a82f52e607e1faca983d9bae1c800b98477e
Mahadev <mahadev.punekar@sazingadigital.com>
2022-07-14 18:57:22 +0530

    update title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageTitleService } from './shared/services/page-title.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngAdminWebPortal';
+export class AppComponent  implements OnInit{
+  title = 'Assimilate - Admin Portal';
+  constructor(private pageTitle: PageTitleService) {
+
+  }
+
+  ngOnInit(): void {
+    this.pageTitle.setTitle({ title: 'Admin Portal', smallTitle: "", isVisible: true});
+  }
 }
